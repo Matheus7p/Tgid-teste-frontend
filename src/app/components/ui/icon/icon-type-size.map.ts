706ce04a80b97dd3bbaa719994b3c0fffbce9f2c
Matheus7p <matheus.costa.7p@gmail.com>
2025-06-13 00:57:46 -0300

    feat: icon mapping

---
+++ src/app/components/ui/icon/icon-type-size.map.ts
@@ -0,0 +1,12 @@
+import { TUiIconTypes } from "./icon-types.enum";
+
+export const uiIconTypeSizeMap = new Map<TUiIconTypes, { width: number, height: number}>([
+  [
+    "bag",
+    { width: 24, height: 24 }
+  ],
+  [
+    "search",
+    { width: 24, height: 24} ,
+  ],
+])
---
+++ src/app/components/ui/icon/icon-type-svg.map.ts
@@ -0,0 +1,13 @@
+import { TUiIconTypes } from "./icon-types.enum";
+import { TUiIconSvgs } from "./icon.svgs.enum";
+
+export const uiIconTypeToSvgMap = new Map<TUiIconTypes, TUiIconSvgs>([
+  [
+    "bag",
+    "/images/icons/bag.svg",
+  ],
+  [
+    "search",
+    "/images/icons/search.svg",
+  ],
+]);
