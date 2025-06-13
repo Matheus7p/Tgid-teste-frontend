5557a8321ad96e48fa1ab00d39328c1bde8961a7
Matheus7p <matheus.costa.7p@gmail.com>
2025-06-13 01:38:02 -0300

    feat: header ui

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,17 +1,14 @@
 import { useProductsContext } from "@/app/context/product.context";
+import { Header } from "../components/header/header.component";
 
 export function Home() {
-  const { products, isLoading } = useProductsContext();
+  const { products } = useProductsContext();
 
-  if (isLoading) {
-    return <div>carregando produtos...</div>;
-  }
-
-  if (products.length === 0) {
-    return <div>nenhum produto disponivel</div>;
-  }
   return (
     <main className="h-screen bg-gray-200">
+      <section className="bg-black h-2/3 ">
+        <Header />
+      </section>
       {products.map((product) => (
         <p className="text-black text-4xl">{product.name}</p>
       ))}
