148297a155fd0b5460a0baf75cc4ba79b49a5b8d
Matheus7p <matheus.costa.7p@gmail.com>
2025-06-14 18:03:12 -0300

    fix: heroSection responsive

--- src/app/components/heroSection/heroSection.component.tsx
+++ src/app/components/heroSection/heroSection.component.tsx
@@ -10,12 +10,14 @@ const _CLOCK = [
 
 const renderClockCard = (): JSX.Element => {
   return(
-    <div className="flex flex-col lg:flex-row justify-between gap-6 items-center mx-4 lg:mx-60" data-aos="fade-up">
+    <div className="flex flex-col lg:flex-row justify-between gap-2 lg:gap-5 items-center mx-4 lg:mx-60" data-aos="fade-up">
       {_CLOCK.map((clock, index) => (
-        <div key={index} className="flex flex-col max-w-80 lg:max-w-96 lg:h-[575px] gap-10 items-center lg:gap-2 rounded-lg bg-zinc-300/50 shadow-2xl p-4 backdrop-blur">
-          <h4 className="text- font-semibold text-2xl">{clock.name}</h4>
-          <img className="w-60 h-60 lg:w-72 lg:h-72 rounded-2xl shadow-lg mb-12" src={clock.image} />
-          <p className="text-center font-semibold text-xl">{clock.description}</p>
+        <div key={index} className="flex flex-row lg:flex-col max-w-60 lg:max-w-96 h-auto lg:h-[575px] items-center lg:gap-2 rounded-lg bg-zinc-300/50 shadow-2xl p-4 backdrop-blur">
+          <div className="flex flex-col w-40">
+            <h4 className="font-semibold lg:text-2xl ml-2 lg:ml-0">{clock.name}</h4>
+            <img className="w-28 h-28 lg:w-72 lg:h-72 rounded-xl lg:rounded-2xl shadow-lg lg:mb-12" src={clock.image} />
+          </div>
+          <p className="text-start font-semibold text-[0.5rem] lg:text-xl max-w-20">{clock.description}</p>
         </div>
       ))}
     </div>
