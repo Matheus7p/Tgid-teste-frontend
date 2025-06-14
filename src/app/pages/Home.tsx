fe76cf2e539f810ece9dd9cfff99614845b0a76b
Matheus7p <matheus.costa.7p@gmail.com>
2025-06-14 14:18:48 -0300

    feat: hero section

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,17 +1,25 @@
 import { useProductsContext } from "@/app/context/product.context";
 import { Header } from "../components/header/header.component";
+import { HeroSection } from "../components/heroSection/heroSection.component";
 
 export function Home() {
   const { products } = useProductsContext();
 
   return (
     <main className="h-screen bg-gray-200">
-      <section className="bg-black h-2/3 ">
+      <section className="bg-black lg:h-2/3 shadow-slate-300 rounded-b-4xl">
         <Header />
       </section>
-      {products.map((product) => (
-        <p className="text-black text-4xl">{product.name}</p>
-      ))}
+    
+      <section className="xl:mt-[-40vh]">
+       <HeroSection />
+      </section>
+
+      <section className="">
+        {products.map((product) => (
+          <p className="text-white text-4xl">{product.name}</p>
+        ))}
+      </section>
     </main>
   );
 }
